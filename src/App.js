34eb5dc9321a6f1875d4793c7c3731778700b389
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2021-03-30 08:49:01 -0500

    *Folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo.svg';
+import './assets/styles/App.css';
+import React from "react";
 
 function App() {
   return (
